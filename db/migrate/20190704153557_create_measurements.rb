3c06535749e28b82e8c67c738b84dc91ba9807a0
kaitofu <yaiag04@gmail.com>
2019-07-05 00:41:42 +0900

    Fix typo

--- db/migrate/20190704153557_create_measurements.rb
+++ db/migrate/20190704153557_create_measurements.rb
@@ -1,9 +1,9 @@
 class CreateMeasurements < ActiveRecord::Migration[5.2]
   def change
     create_table :measurements do |t|
-      t.integer, :weight
-      t.integer, :fat_percentage
-      t.datet :measured_on
+      t.integer :weight
+      t.integer :fat_percentage
+      t.date :measured_on
 
       t.timestamps
     end
